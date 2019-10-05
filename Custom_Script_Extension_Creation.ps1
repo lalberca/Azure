d0439ba10f3670bd1f5690c1efae6256bd793c41
lalberca <39997545+lalberca@users.noreply.github.com>
2019-10-05 11:28:37 -0500

    Add files via upload

---
+++ Custom_Script_Extension_Creation.ps1
@@ -0,0 +1,25 @@
+﻿#Se crea objeto de configuración
+$customConfig = @{
+  "fileUris" = (,"https://raw.githubusercontent.com/lalberca/Azure/master/Install_IIS.ps1");
+  "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File Install_IIS.ps1"
+}
+
+#Se obtiene información acerca del conjunto de escalado
+$vmss = Get-AzVmss `
+          -ResourceGroupName "PLATAFORMAresourcegroup" `
+          -VMScaleSetName "WEBscaleset"
+
+#Agregar Custom Script Extension
+$vmss = Add-AzVmssExtension `
+  -VirtualMachineScaleSet $vmss `
+  -Name "customScript" `
+  -Publisher "Microsoft.Compute" `
+  -Type "CustomScriptExtension" `
+  -TypeHandlerVersion 1.9 `
+  -Setting $customConfig
+
+#Se actualiza el conjunto de escalado y se aplica el Custom Script Extension las instancias de las VMs
+Update-AzVmss `
+  -ResourceGroupName "PLATAFORMAresourcegroup" `
+  -Name "WEBscaleset" `
+  -VirtualMachineScaleSet $vmss
